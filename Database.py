39dade963018adb988dc846f5e24f673991b4d4e
Philippe Sorignon <philippe.sorignon@protonmail.com>
2019-07-31 12:22:00 +0200

    More methods for the Database class

--- Database.py
+++ Database.py
@@ -32,14 +32,19 @@ class Database:
         finally:
             connection_instance.close()
 
+
     def connect(self):
+        """Connection to MariaDB"""
         self.connection_instance = pymysql.connect(host=self.database_server_ip, \
         user=self.database_user_name, password=self.database_user_password, \
         db=self.database_name, \
-        charset=self.char_set, cursorclass=self.cusror_type)
+        charset=self.char_set, cursorclass=self.cusror_type, autocommit=True)
+
 
     def disconnect(self):
+        """Disconnect from MariaDB"""
         self.connection_instance.close()
+
 
     def create_tables(self):
         """Create all the tables"""
@@ -59,4 +64,103 @@ class Database:
             cursor_instance.execute(sql_statement)
 
         except Exception as e:
+            print("Exeception occured:{}".format(e))
+
+
+    def save_product(self, name, nutri_score, url, stores):
+        """Save a product in the database"""
+        try:
+            cursor_instance = self.connection_instance.cursor()
+            sql_statement = "INSERT INTO Product (name, nutri_score, url, saved)\
+             VALUES ('"+name+"', '"+nutri_score+"', '"+url+"', '0')"
+            cursor_instance.execute(sql_statement)
+
+            for current_store in range(len(stores)):
+                if not self.does_store_exists(stores[current_store]):
+                    self.save_store(stores[current_store])
+                self.make_product_store_association(self.get_product_id(name),\
+                 self.get_store_id(stores[current_store]))
+
+        except Exception as e:
+            print("Exeception occured:{}".format(e))
+
+
+    def save_store(self, name):
+        """Save a store in the database"""
+        try:
+            cursor_instance = self.connection_instance.cursor()
+            sql_statement = "INSERT INTO Store (name) VALUES('"+name+"')"
+            cursor_instance.execute(sql_statement)
+
+        except Exception as e:
+            print("Exeception occured:{}".format(e))
+
+
+    def make_product_store_association(self, id_product, id_store):
+        """Save a product/store association"""
+        try:
+            cursor_instance = self.connection_instance.cursor()
+            sql_statement = "INSERT INTO AssociationProductStore\
+             VALUES('"+str(id_product)+"', '"+str(id_store)+"')"
+            cursor_instance.execute(sql_statement)
+
+        except Exception as e:
+            print("Exeception occured:{}".format(e))
+
+
+    def get_products(self):
+        """Return a dictionnary of all the products saved in the database"""
+        try:
+            cursor_instance = self.connection_instance.cursor()
+            sql_statement = "SELECT * FROM Product"
+            cursor_instance.execute(sql_statement)
+            result = cursor_instance.fetchall()
+
+        except Exception as e:
+            print("Exeception occured:{}".format(e))
+
+
+    def does_store_exists(self, store_name):
+        """Check if the store is already saved in the database"""
+        try:
+            cursor_instance = self.connection_instance.cursor()
+            sql_statement = "SELECT * FROM Store WHERE name='"+store_name+"'"
+            cursor_instance.execute(sql_statement)
+            result = cursor_instance.fetchone()
+            if result is None:
+                return False
+            else:
+                return True
+
+        except Exception as e:
+            print("Exeception occured:{}".format(e))
+
+
+    def get_product_id(self, product_name):
+        """Return the product ID"""
+        try:
+            cursor_instance = self.connection_instance.cursor()
+            sql_statement = "SELECT id FROM Product WHERE name='"+product_name+"'"
+            cursor_instance.execute(sql_statement)
+            result = cursor_instance.fetchone()
+
+            return result['id']
+
+        except Exception as e:
+            print('get product id')
+            print("Exeception occured:{}".format(e))
+
+
+    def get_store_id(self, store_name):
+        """Return the store ID"""
+        try:
+            cursor_instance = self.connection_instance.cursor()
+            sql_statement = "SELECT id FROM Store WHERE name='"+store_name+"'"
+            cursor_instance.execute(sql_statement)
+            result = cursor_instance.fetchone()
+
+            return result['id']
+
+        except Exception as e:
+            print('get store id')
             print("Exeception occured:{}".format(e))
